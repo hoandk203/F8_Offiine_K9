be76ee77920be4cfd7470eee1fb868946b2f674e
hoandk203 <hoanyttv@gmail.com>
2024-07-28 19:56:09 +0700

    bai tap ve nha buoi 31

---
+++ Day31/BTVN/js/todo.js
@@ -0,0 +1,54 @@
+var inputAdd= document.querySelector('.input-add')
+var btnAdd= document.querySelector('.btn-add')
+var tasksEl= document.querySelector('.tasks')
+
+btnAdd.addEventListener("click", function(e){
+    e.preventDefault();
+    tasksEl.innerHTML += `<div class="task">
+                    <p>${inputAdd.value}</p>
+                    <div class="action">
+                        <i class="btn-update fa-regular fa-pen-to-square"></i>
+                        <i class="btn-delete fa-regular fa-trash"></i>
+                    </div>
+                </div>`
+    inputAdd.value= '';
+    action();
+})
+
+/////////////////////////////////////////////////////////
+function action(){
+    var tasks = document.querySelectorAll('.task');
+    tasks.forEach(function(task) {
+        task.addEventListener("click", function(e) {
+            if (e.target.classList.contains('btn-delete')) {
+                task.remove();
+            }else if(e.target.classList.contains('btn-update')){
+                var taskValue= task.querySelector('p').innerText
+                task.outerHTML= `<form action="">
+                        <input type="text" class="input-update" value="${taskValue}">
+                        <button class="btn-enter-update">Edit task</button>
+                    </form>`
+
+                var btnEnterUpdate= document.querySelector('.btn-enter-update')
+                btnEnterUpdate.addEventListener("click", function(eventEnterUpdate){
+                    eventEnterUpdate.preventDefault();
+                    var inputUpdateValue= document.querySelector('.input-update').value
+                    var formUpdate= document.querySelector('.tasks form')
+                    formUpdate.outerHTML= `<div class="task">
+                            <p>${inputUpdateValue}</p>
+                            <div class="action">
+                                <i class="btn-update fa-regular fa-pen-to-square"></i>
+                                <i class="btn-delete fa-regular fa-trash"></i>
+                            </div>
+                        </div>`
+                    action();
+                })
+            }
+        });
+    });
+
+
+}
+
+
+action();
